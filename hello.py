ed539db3c52f5ede5340e62c71f297ae6f628946
Luis David Miranda <ldmiranda123@gmail.com>
2017-03-02 11:10:23 -0400

    Terminado

--- hello.py
+++ hello.py
@@ -1,13 +1,17 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, jsonify
+from flask import render_template
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/review', methods=['GET', 'POST'])
+def review():
+    if request.method == 'POST':
+        return jsonify("ok")
+        
+    elif request.method == 'GET':
+        return "GET"
 
 if __name__ == "__main__":
     app.run()
